54dde3795a290ad3986d162737cf3d7cf81733f6
nasa9084 <nasa9084@live.jp>
2016-02-19 17:28:41 +0900

    modify not to take arguments from cmd line

--- euler-0029.py
+++ euler-0029.py
@@ -3,17 +3,11 @@
 
 ls = []
 add = ls.append
-import sys
-argv = sys.argv
-argc = len(argv)
-if not argc==3:
-    print("Usage: python {} <int from> <int to>\n".format(argv[0]))
-    quit()
-else:
-    frm = int(argv[1])
-    to = int(argv[2])
-    for a in range(frm, to+1):
-        for b in range(frm, to+1):
-            add(a**b)
 
-    print("Answer: " + str(len(list(set(ls)))))
+frm = 2
+to = 100
+for a in range(frm, to+1):
+    for b in range(frm, to+1):
+        add(a**b)
+
+print("Answer: " + str(len(list(set(ls)))))
