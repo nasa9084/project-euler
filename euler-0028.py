6616b99a6d2173aabfccabedcef127357a41b440
nasa9084 <nasa9084@live.jp>
2016-02-19 17:25:15 +0900

    modify not to take arguments from cmd line

--- euler-0028.py
+++ euler-0028.py
@@ -12,21 +12,7 @@ def spiral(n):
             cornersum += here
         return spiral(n-1) + cornersum
 
-import sys
-argv = sys.argv
-argc = len(argv)
-if argc>3:
-    print("Usage: python {} [rev|size] <int>\n".format(argv[0]))
-    quit()
-elif argc==2:
-    rev = int(argv[1])
-elif argc==3:
-    if argv[1]=="rev":
-        rev = int(argv[1])
-    elif argv[1]=="size":
-        size = int(argv[2])
-        rev = (size+1)/2
-    else:
-        print("Usage: python {} [rev|size] <int>\n".format(argv[0]))
-        quit()
+
+size = 1001
+rev = (size+1)//2
 print("Answer: " + str(spiral(rev)))
